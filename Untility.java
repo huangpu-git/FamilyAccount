4f0f53153d78188c28a10be6ef3da9e9f5715aad
huangpu <759727541@qq.com>
2020-07-06 23:24:30 +0800

    develop Untility

--- Untility.java
+++ Untility.java
@@ -7,6 +7,7 @@
 import java.util.Scanner;
 public class Untility {
 	private static Scanner scanner = new Scanner(System.in);
+
 	/**
 	用户界面菜单的选择，用户输入字符，返回输入的字符
 	*/
@@ -21,10 +22,83 @@ public class Untility {
 			}else break;
 		}
 		return c;	
+		}
 	}
 
-}
-public static int readNumber(){
+	/**
 	
-	System.out.println("My_tes_git")
-}
+	用于收入和支出金额的输入。该方法从键盘读取一个不超过4位长度的整数，并将其作为方法的返回值。
+
+	*/
+
+	public static int readNumber()
+	{
+		int n;
+		for (; ; )
+		{
+			String str = readKeyBoard(4);
+			try
+			{
+				n = Integer.parseInt(str);
+				break;
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("数字输入有误，请重新输入：")
+			}
+		}
+		return n;
+	}
+
+	/**
+	用于收入和支出说明的输入。该方法从键盘读取一个不超过8位长度的字符串，并将其作为方法的返回值。
+	*/
+
+	public static String readString() 
+	{
+		String str = readKeyBoard(8);
+		return str;
+
+	}
+
+	/**
+	用于确认选择的输入。该方法从键盘读取‘Y’或’N’，并将其作为方法的返回值。
+	*/
+
+	public static char readConfirmSelection() {
+		char c;
+		for (; ; )
+		{
+			String str = readkeyBoard(1).toUpperCase();
+			c = str.charAt(0);
+			if (c == 'Y' || c == "N")
+			{
+				break;
+			}else{
+				System.out.println("输入有误，请重新输入：");
+			}
+		}
+		return c;
+	}
+
+	/**
+
+	用于读取键盘输入的内容
+
+	*/
+
+	public static String readKeyBoard(int limit){
+		String line = "";
+
+		while (scanner.hasNext())
+		{
+			line = scanner.nextLine();
+			if (line.length() < 1 || line.length() > limit) 
+			{
+				System.out.println("输入长度（不大于" + limit + ") 错误，请重新输入：" );
+				continue;
+			}
+		}
+
+		return line;
+	}
