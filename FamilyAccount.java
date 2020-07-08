ce5f144487790c4168da9811c5433f80c2a4f736
huangpu <759727541@qq.com>
2020-07-08 22:56:46 +0800

    finish project

--- FamilyAccount.java
+++ FamilyAccount.java
@@ -1,7 +1,10 @@
 class FamilyAccount
+	
 {
 	public static void main(String[] args) 
 	{
+		String details = "收支\t账户金额\t收支金额\t说    明\n";
+		int balance = 10000;
 		Boolean isFlag = true;
 		while (isFlag)
 		{
@@ -17,16 +20,35 @@ class FamilyAccount
 			switch (key)
 			{
 			case '1':
-				System.out.println("收支明细");
+				System.out.println("-----------------当前收支明细记录-----------------");
+				System.out.println(details);
+				System.out.println("--------------------------------------------------");
 				break;
 			case '2':
-				System.out.println("登记收入");
+				 System.out.print("本次收入金额：");
+				int amount1 = Utility.readNumber();
+				System.out.print("本次收入说明：");
+				String desc1 = Utility.readString();
+
+				balance += amount1;
+				details += "收入\t" + balance + "\t\t" + amount1 + "\t\t" + desc1 + "\n";
+				System.out.println("---------------------登记完成---------------------");
 				break;
 			case '3':
-				System.out.println("登记支出");
+				System.out.print("本次支出金额：");
+				int amount2 = Utility.readNumber();
+				System.out.print("本次支出说明：");
+				String desc2 = Utility.readString();
+
+				balance -= amount2;
+				details += "支出\t" + balance + "\t\t" +
+						   amount2 + "\t\t" + desc2 + "\n";
+				System.out.println("---------------------登记完成---------------------");
 				break;
 			case '4':
-				System.out.println("退  出");
+				System.out.println("确认是否退出：Y/N");
+				char isExit = Utility.readConfirmSelection();
+				if (isExit == 'Y') isFlag = false;
 				break;
 			}
 		}
